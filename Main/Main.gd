03673ae41a032c71ee3e9605531eca76015a5fe0
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-11 13:55:08 -0400

    Add Level changing capabilities

--- Main/Main.gd
+++ Main/Main.gd
@@ -2,8 +2,10 @@ extends Node
 
 onready var _player : KinematicBody2D = $Player
 onready var _music : AudioStreamPlayer = $Music
-onready var _doot : Area2D = $Level_Door
+onready var _door : Area2D = $Level_Door
 var _between : bool = false
+var _not_first : bool = false
+var _last : Node = null
 
 func _ready():
 	_music.play()
@@ -15,20 +17,20 @@ func _on_Player_dead():
 
 func _load_level():
 	var _level : PackedScene
-	var _Level : Node
-	if _Level != null:
-		pass
-		#_Level.queue_free()
+	if _not_first == true:
+		_last.queue_free()
 	if not _between:
 		_level = load("res://Levels/Level" + str(Variables.level) + ".tscn")
 	if _between:
 		_level = load("res://Levels/In_between.tscn")
-	_Level = _level.instance()
+	var _Level : Node = _level.instance()
+	_last = _Level
 	var _start = _Level.get_node("Start")
 	var _exit = _Level.get_node("Exit")
+	_not_first = true
 	add_child(_Level)
 	_player.position = _start.global_position
-	_doot.position = _exit.global_position
+	_door.position = _exit.global_position
 	
 
 func _on_Level_Door_entered():
