cfcd265ae1d9bcac85c20e0cc16c3056986ebe43
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-24 11:58:55 -0400

    Add door smash sfx

--- Scenery/Door.gd
+++ Scenery/Door.gd
@@ -1,8 +1,10 @@
 extends StaticBody2D
 
 onready var sprite : AnimatedSprite = $AnimatedSprite
+onready var _smash : AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func hit(_damage):
+	_smash.play()
 	sprite.animation = "Open"
 	set_collision_layer_bit(1, false)
 	set_collision_mask_bit(1, false)
