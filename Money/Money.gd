529b6e38b193414c2b7c43ecd15acf22ff04b168
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-24 11:32:28 -0400

    Add pickup sfx

--- Money/Money.gd
+++ Money/Money.gd
@@ -1,17 +1,19 @@
 extends RigidBody2D
 
-onready var flipper : AnimationPlayer = $AnimationPlayer
-onready var sprite  : Sprite= $Sprite
+onready var _flipper : AnimationPlayer = $AnimationPlayer
+onready var _sprite  : Sprite = $Sprite
+onready var _ding : AudioStreamPlayer = $AudioStreamPlayer
+onready var _collision : CollisionShape2D = $CollisionShape2D
 var cell : bool
 
 func _ready():
 	randomize()
 	apply_impulse(Vector2(0,0), Vector2(rand_range(-100,100),rand_range(100,200)))
-	flipper.play("Flip")
+	_flipper.play("Flip")
 	if cell == true:
-		sprite.animation = "Cell"
+		_sprite.animation = "Cell"
 	else:
-		sprite.animation = "Coin"
+		_sprite.animation = "Coin"
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
@@ -21,4 +23,8 @@ func _on_Area2D_body_entered(body):
 			Variables.player_damage += 5
 			Variables.health += 5
 			Variables.max_health += 5
+		_ding.play()
+		hide()
+		_collision.set_deferred("disabled", true)
+		yield(get_tree().create_timer(0.15), "timeout")
 		queue_free()
